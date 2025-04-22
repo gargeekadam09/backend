96e59ab0ffeccffde18468e57783ac3ce2a4f046
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-04-22 17:04:03 +0100

    removed frontend url origin cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://client-rust-phi.vercel.app/', // Your client's address
+   // Your client's address
   credentials: true
    //origin: '*'
 }));
