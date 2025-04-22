f0a515c05ffd5004e98d4c512748cb89b82d9cb6
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-04-22 16:01:31 +0100

    added env

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-  host: 'om-security.c3qmska06vqw.eu-north-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'Gargee09',  
-  database: 'om-security',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const customerRoutes = require('./routes/customerRoutes');
 const saleRoutes = require('./routes/saleRoutes');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
